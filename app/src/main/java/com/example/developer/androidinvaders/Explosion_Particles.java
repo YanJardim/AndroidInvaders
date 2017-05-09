25c3ee75c85aeb52f2edcb858451b7abc2d65415
DEVELOPER <developer@LADMAC0103.local>
2017-05-08 21:15:52 -0300

    Adicionando a classe das particulas

---
+++ app/src/main/java/com/example/developer/androidinvaders/Explosion_Particles.java
@@ -0,0 +1,63 @@
+package com.example.developer.androidinvaders;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+/**
+ * Created by developer on 17/04/17.
+ */
+
+public class Explosion_Particles extends GameObject {
+    public static final int ESTADO_VIDA = 0;
+    public static final int ESTADO_MORTA = 1;
+
+    Particles[] particles;
+
+    public int estado = ESTADO_MORTA;
+
+    int tamanho = 0;
+
+    public boolean isVida(){
+        return estado == ESTADO_VIDA;
+    }
+    public boolean isMorta(){
+        return estado == ESTADO_MORTA;
+    }
+    public void loadParticulas(int numeroDeParticulas,
+                          int x,int y){
+        estado = ESTADO_VIDA;
+        particles = new Particles[numeroDeParticulas];
+        for(int i = 0; i< particles.length; i++){
+            Particles p = new Particles(x,y);
+            particles[i] = p;
+        }
+        tamanho = numeroDeParticulas;
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        if(isVida()){
+            boolean morta = true;
+            for(int i = 0; i< particles.length; i++){
+                if(particles[i].isVida()){
+                    particles[i].update(deltaTime);
+                    morta = false;
+                }
+            }
+            if(morta){
+                estado = ESTADO_MORTA;
+            }
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas, Paint paint) {
+        if(isVida()){
+            for(int i = 0; i< particles.length; i++) {
+                if (particles[i].isVida()) {
+                    particles[i].draw(canvas, paint);
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/developer/androidinvaders/Particles.java
@@ -0,0 +1,92 @@
+package com.example.developer.androidinvaders;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import java.util.Random;
+
+/**
+ * Created by developer on 17/04/17.
+ */
+
+public class Particles extends GameObject {
+    public static final int ESTADO_VIDA = 0;
+    public static final int ESTADO_MORTA = 1;
+
+    public static final int TEMPO_DE_VIDA = 200;
+    public static final int TAMANHO_MAXIMO = 15;
+    public static final int VELOCIDADE_MAXIMA = 20;
+
+    int estado;
+
+    double xv,yv;
+
+    int idade;
+    int tempoDeVida;
+    int cor;
+    int alpha,red,green,blue;
+
+    public boolean isVida(){
+        return estado == ESTADO_VIDA;
+    }
+    public boolean isMorta(){
+        return estado == ESTADO_MORTA;
+    }
+    public Particles(float x, float y){
+        this.x = x;
+        this.y = y;
+
+        estado = ESTADO_VIDA;
+        Random r = new Random();
+        width = r.nextInt(TAMANHO_MAXIMO) + 1;
+        height = width;
+
+        tempoDeVida = TEMPO_DE_VIDA;
+        idade = 0;
+        alpha = 255;
+        red = r.nextInt(255);
+        green = r.nextInt(255);
+        blue = r.nextInt(255);
+
+        cor = Color.argb(alpha,red,
+                green,blue);
+
+
+        xv = r.nextDouble()* VELOCIDADE_MAXIMA *2-
+                VELOCIDADE_MAXIMA;
+        yv = r.nextDouble()* VELOCIDADE_MAXIMA *2-
+                VELOCIDADE_MAXIMA;
+
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        if(isVida()){
+            x += xv * deltaTime * 0.1;
+            y += yv * deltaTime * 0.1;
+
+            alpha -= 2;
+            idade++;
+            if(idade >= tempoDeVida ||
+                    alpha <= 0){
+                estado = ESTADO_MORTA;
+            }else{
+                cor = Color.argb(alpha,red,
+                        green,blue);
+            }
+
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas, Paint paint) {
+        super.draw(canvas, paint);
+        paint.setColor(cor);
+        paint.setAlpha(alpha);
+        canvas.drawRect(x,y,
+                x+width,y+height,paint);
+        paint.reset();
+    }
+}
